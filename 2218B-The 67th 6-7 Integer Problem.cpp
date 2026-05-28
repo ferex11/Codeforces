9c4417a2849c03f4bee38462a92dc5ed235bf967
Sujeet Kumar <sujeet901626@gmail.com>
2026-05-28 19:42:31 +0530

    Create 2218B-The 67th 6-7 Integer Problem.cpp

---
+++ 2218B-The 67th 6-7 Integer Problem.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int sum = 0;
+    int mx = INT_MIN;
+    for (int i = 0; i < 7; i++) {
+      int x;
+      cin >> x;
+      sum += x;
+      mx = max(mx, x);
+    }
+    cout << 2 * mx - sum << "\n";
+  }
+  return 0;
+}
