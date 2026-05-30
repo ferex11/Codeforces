5c49b4ad6231d6dcf29b6d21435526f749f4de0c
Sujeet Kumar <sujeet901626@gmail.com>
2026-05-30 06:44:12 +0530

    Create 281A-Word Capitalization.cpp

---
+++ 281A-Word Capitalization.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define pb push_back
+
+void solve() {
+  string s;
+  cin >> s;
+  s[0] = toupper(s[0]);
+  cout << s << endl;
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  solve();
+  return 0;
+}
