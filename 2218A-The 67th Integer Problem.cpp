7b1add865ab2ffdad50a07463793e33eec85f376
Sujeet Kumar <sujeet901626@gmail.com>
2026-06-03 06:22:53 +0530

    Create 2218A-The 67th Integer Problem.cpp

---
+++ 2218A-The 67th Integer Problem.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define pb push_back
+#define inf 1e18
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+void solve() {
+  int x;
+  cin >> x;
+  if (x + 1 > 67) cout << x << "\n";
+  else cout << x + 1 << "\n";
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
