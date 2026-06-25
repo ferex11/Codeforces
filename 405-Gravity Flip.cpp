dda325699562d5731986b603bcbcd6918ff4a3ad
Sujeet Kumar <sujeet901626@gmail.com>
2026-06-25 06:12:12 +0530

    Create 405-Gravity Flip.cpp

---
+++ 405-Gravity Flip.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  for (int x : a) {
+    cout << x << " ";
+  }
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
