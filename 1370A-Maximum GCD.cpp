596280b8cef7fe3ac3d442625e8591869a1407aa
Sujeet Kumar <sujeet901626@gmail.com>
2026-06-26 12:03:53 +0530

    Create 1370A-Maximum GCD.cpp

---
+++ 1370A-Maximum GCD.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define sp ' '
+#define pb push_back
+#define inf 1e18
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+void solve() {
+  int n;
+  cin >> n;
+  cout << n / 2 << endl;
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
