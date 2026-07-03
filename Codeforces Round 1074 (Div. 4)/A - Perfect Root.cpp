ae975752fb6850688609e742602038e5fc7ab32c
Sujeet Kumar <sujeet901626@gmail.com>
2026-07-03 06:01:14 +0530

    Create A - Perfect Root.cpp

---
+++ Codeforces Round 1074 (Div. 4)/A - Perfect Root.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        for (int i = 1; i <= n; i++) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
