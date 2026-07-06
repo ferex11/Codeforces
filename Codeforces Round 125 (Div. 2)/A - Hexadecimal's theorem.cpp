6bddf1658c6fd0bc7c575b3666270c9b1d377134
Sujeet Kumar <sujeet901626@gmail.com>
2026-07-06 13:58:20 +0530

    Create A - Hexadecimal's theorem.cpp

---
+++ Codeforces Round 125 (Div. 2)/A - Hexadecimal's theorem.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+static long long MOD = 1e9;
+#define ll long long
+#define endl "\n"
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << 0 << " " << 0 << " " << n << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  solve();
+  return 0;
+}
