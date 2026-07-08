38ef47052b1bb4b970ad3a0ab510f50a496ca677
Sujeet Kumar <sujeet901626@gmail.com>
2026-07-08 17:27:37 +0530

    Create A - Little Elephant and Function.cpp

---
+++ Codeforces Round 136 (Div. 2)/A - Little Elephant and Function.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << n << " ";
+    for (int i = 1; i < n; i++)
+        cout << i << " ";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
