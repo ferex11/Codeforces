285e3a4acd406dcfeafc77d505f3dff9c12ce8b8
Sujeet Kumar <sujeet901626@gmail.com>
2026-07-31 06:51:18 +0530

    Create A - How Much Does Daytona Cost?.cpp

---
+++ Codeforces Round 900 (Div. 3)/A - How Much Does Daytona Cost?.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k;
+    cin >> n >> k;
+    bool found = false;
+    for (int i = 0; i < n; i++) {
+      int x;
+      cin >> x;
+      if (x == k) {
+        found = true;
+      }
+    }
+    if (found) {
+      cout << "YES\n";
+    } else {
+      cout << "NO\n";
+    }
+  }
+  return 0;
+}
