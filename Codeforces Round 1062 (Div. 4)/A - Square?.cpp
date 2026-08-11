101b0784223ee114866980c5392102f4f7ffdcd2
Sujeet Kumar <sujeet901626@gmail.com>
2026-08-11 16:01:53 +0530

    Update and rename A - Square?.cpp to A - Square?.cpp

---
+++ Codeforces Round 1062 (Div. 4)/A - Square?.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  if (a == b && a == c && a == d) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
