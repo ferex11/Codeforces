3751ead6ce66b4d9e00be83f1212630ba8e5000e
Sujeet Kumar <sujeet901626@gmail.com>
2026-08-17 15:35:50 +0530

    Create C - Number of Minimums on a Segment.cpp

---
+++ EDU/Course/2/Lesson/4/1/Practice/C - Number of Minimums on a Segment.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define pb push_back
+
+struct Node {
+	int mn;
+	int cnt;
+};
+
+class SegmentTree {
+private:
+	vector<Node> seg;
+	vector<int> arr;
+	int n;
+
+	Node merge(Node& a, Node& b) {
+		if (a.mn < b.mn)
+			return a;
+		if (b.mn < a.mn)
+			return b;
+		return {a.mn, a.cnt + b.cnt};
+	}
+
+	void build(int ind, int low, int high) {
+		if (low == high) {
+			seg[ind] = {arr[low], 1};
+			return;
+		}
+		int mid = low + (high - low) / 2;
+		build(2 * ind + 1, low, mid);
+		build(2 * ind + 2, mid + 1, high);
+		seg[ind] = merge(seg[2 * ind + 1], seg[2 * ind + 2]);
+	}
+
+	Node query(int ind, int low, int high, int l, int r) {
+		if (low > r || high < l)
+			return {INT_MAX, 0};
+		if (l <= low && high <= r)
+			return seg[ind];
+		int mid = low + (high - low) / 2;
+		Node leftRes = query(2 * ind + 1, low, mid, l, r);
+		Node rightRes = query(2 * ind + 2, mid + 1, high, l, r);
+		return merge(leftRes, rightRes);
+	}
+
+	void update(int ind, int low, int high, int pos, int val) {
+		if (low == high) {
+			seg[ind] = {val, 1};
+			arr[pos] = val;
+			return;
+		}
+		int mid = low + (high - low) / 2;
+		if (pos <= mid)
+			update(2 * ind + 1, low, mid, pos, val);
+		else 	
+			update(2 * ind + 2, mid + 1, high, pos, val);
+		seg[ind] = merge(seg[2 * ind + 1], seg[2 * ind + 2]);
+	}
+public:
+	SegmentTree(vector<int>& input) {
+		arr = input;
+		n = arr.size();
+		seg.resize(4 * n);
+		build(0, 0, n - 1);
+	}
+
+	Node getMin(int l, int r) {
+		return query(0, 0, n - 1, l, r);
+	}
+
+	void setValue(int pos, int val) {
+		update(0, 0, n - 1, pos, val);
+	}
+};
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m;
+	cin >> n >> m;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) cin >> v[i];
+	SegmentTree smt(v);
+	while (m--) {
+		int type;
+		cin >> type;
+		if (type == 1) {
+			int pos, val;
+			cin >> pos >> val;
+			smt.setValue(pos, val);
+		} else {
+			int l, r;
+			cin >> l >> r;
+			Node ans = smt.getMin(l, r - 1);
+			cout << ans.mn << " " << ans.cnt << endl;
+		}
+	}
+	return 0;
+}
